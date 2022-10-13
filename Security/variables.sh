8473215f4e8863834d37154b691f3ef7479049d0
Tomas Pesek <pesekt@gmail.com>
2022-10-13 22:44:45 +0200

    deleted 2 chapters

--- Security/variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-RG_NAME="<resource group name to create>"
-ACR_NAME="<azure container registry name to create>"
-SP_NAME="<service principal name to create>"
-RG_LOCATION="<resource group location e.g. eastus, canadacentral>"
